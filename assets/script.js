6cde15b78be860b14e6766e15ec2180c0756b3ae
Hunter Padgett <gizmokabanjo@gmail.com>
2022-07-02 10:45:49 -0400

    worked on JS

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,34 @@
+var x = moment();
+
+// assign month, day year to id currentDay
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+console.log(today)
+
+// get hour of day
+var hourOfDay = parseInt(moment().hour());
+console.log(hourOfDay)
+
+// change color of 
+if (hourOfDay > x) {
+  $('#colorTime').css('background-color', '#d3d3d3');
+} else if (hourOfDay < x) {
+  $('#colorTime').css('background-color', '#77dd77');
+} else {
+  $('#colorTime').css('background-color', '#ff6961');
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
